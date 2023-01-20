acd768002b7783b5a451accbd130628873624395
Manthan Godha <72178048+godhamanthan@users.noreply.github.com>
2023-01-20 18:21:04 +0530

    Create 05_59_MaxSubArray.java

---
+++ 05_59_MaxSubArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+    int currMaxSum = nums[0];
+    int maxSumSoFar = nums[0];
+    for(int i=1; i<nums.length;i++){
+        currMaxSum = Math.max(currMaxSum+nums[i], nums[i]);
+        maxSumSoFar = Math.max(maxSumSoFar,currMaxSum);
+    }
+    return maxSumSoFar;
+  }
+}
